1b54ae299f980f31e2ad000ee7d2985244e5b84e
rscfeitosa <rodolpho.feitosa@aluno.faculdadeimpacta.com.br>
2024-12-12 14:23:59 -0300

    def

---
+++ Exe.scripts/Funções.py
@@ -0,0 +1,20 @@
+def adicao(x,y):
+    resultado = x+y
+    print(resultado)
+
+def subtracao(x,y):
+    resultado = x-y
+    print(resultado)
+
+def divizao(x,y):
+    resultado = x/y
+    print(resultado)
+
+def multiplicacao(x,y):
+    resultado = (x*y)
+    print(resultado)
+
+multiplicacao(5,10)
+divizao(10,5)
+subtracao(10,5)
+adicao(10,5)
